9fbe564c2aea1bdaf509172c2c1f3e9ed00f68e6
Eugenia <j.voloshchenko@gmail.com>
2022-08-11 20:41:57 +0300

    Add navigation to product and style it

--- components/Products.js
+++ components/Products.js
@@ -1,13 +1,16 @@
 import { ProductStyles } from "../styles/productStyle";
+import Link from "next/link";
 
 export default function Product({ product }) {
   //Extract the info from props
-  const { Title, price, image, description } = product.attributes;
+  const { Title, price, image, description, slug } = product.attributes;
   return (
     <ProductStyles>
-      <div>
-        <img src={image.data.attributes.formats.small.url} alt="" />
-      </div>
+      <Link href={`/product/${slug}`}>
+        <div>
+          <img src={image.data.attributes.formats.small.url} alt="" />
+        </div>
+      </Link>
       <h2>{Title}</h2>
       <h3>{description}</h3>
       <h3>{price}</h3>
